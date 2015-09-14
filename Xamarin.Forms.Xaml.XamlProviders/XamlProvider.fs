4e7b3d5d8e94e25f3507bec5319aeafa466a31c7
Stephane Delcroix <stephane@delcroix.org>
2015-09-14 15:37:24 +0200

    wip

--- Xamarin.Forms.Xaml.XamlProviders/XamlProvider.fs
+++ Xamarin.Forms.Xaml.XamlProviders/XamlProvider.fs
@@ -10,6 +10,7 @@ open Microsoft.FSharp.Core.CompilerServices
 open ProviderImplementation.ProvidedTypes
 
 open Xamarin.Forms.Build.Tasks
+open Xamarin.Forms.Xaml
 
 [<TypeProvider>]
 type XamlTypeProvider (config: TypeProviderConfig) as this =
@@ -25,6 +26,11 @@ type XamlTypeProvider (config: TypeProviderConfig) as this =
         match XamlGTask.ParseXaml stream with
             |null, null, null, null -> false
             |rootType, ns, baseType, namesAndTypes -> rootType = xClass
+    
+    let getType name ns assembly =
+        match Xamarin.Forms.Xaml.XamlParser.GetElementType (XmlType (ns, name, null), null, asm) with
+        |t, null -> t
+        |_, ex -> null
     
     let createFields (namesAndType: IDictionary<string,CodeDom.CodeTypeReference>) =
         namesAndType |> List.ofSeq |> List.map (fun nameAndType ->
@@ -42,8 +48,9 @@ type XamlTypeProvider (config: TypeProviderConfig) as this =
         let allXamlFiles = Directory.EnumerateFiles (config.ResolutionFolder, "*.xaml", SearchOption.AllDirectories) |> List.ofSeq
         let xamlFile = allXamlFiles |>  List.filter (fun path -> isMatchingXClass path xClass) |> List.head
         use stream = File.OpenText xamlFile
-        let rootType, ns, baseType, namesAndType = XamlGTask.ParseXaml stream
-        let customType = ProvidedTypeDefinition (asm, ns, typeName, Some typeof<obj>, IsErased = false)
+        let rootType, nsuri, baseCodeReference, namesAndType = XamlGTask.ParseXaml stream
+        let baseType = getType baseCodeReference.BaseType "http://xamarin.com/schemas/2014/forms" asm
+        let customType = ProvidedTypeDefinition (asm, ns, typeName, Some baseType, IsErased = false)
 
         namesAndType |> createFields |> List.iter (fun field -> customType.AddMember field)
         customType.AddMember (createCtor ())
